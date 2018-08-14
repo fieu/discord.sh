584ac169b81b906e2cca7b6593e85723037beef1
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-14 21:16:03 +0000

    make test script sexy

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 
-# TODO - automatically detect and run tests using builtins
+for filename in tests/*.bats; do
+    echo -ne "\n  -- bats: ${filename}\n\n"
 
+    bats "${filename}"
+    exitcode=$?
 
-# assorted things (most of which should fail)
-
-echo -ne "\n  -- bats: tests/etc.bats\n\n"
-bats tests/etc.bats
-
-etc_code=$?
-[[ "$etc_code" -ne 0 ]] && echo -ne "\n  -- FATAL: tests/etc.bats failed with code: ${etc_code}\n\n" && exit ${etc_code}
-
-
-# test 'say' command
-
-echo -ne "\n  -- bats: tests/say.bats\n\n"
-bats tests/say.bats
-
-say_code=$?
-[[ "$say_code" -ne 0 ]] && echo -ne "\n  -- FATAL: tests/say.bats failed with code: ${say_code}\n\n" && exit ${say_code}
-
+    [[ "${exitcode}" -ne 0 ]] && \
+        echo -ne "\n  -- FATAL: ${filename} failed with code: ${exitcode}\n\n" && \
+        exit ${exitcode}
+done
 
 # all ok
 
 echo -ne "\n  -- OK: all tests passed\n\n"
-exit 0
+
