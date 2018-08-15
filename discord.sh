d9f06e3ac1d94878ffeed1d2fc96c981ab0a53e8
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-15 19:21:53 +0000

    I BROKE EVERYTHING

--- discord.sh
+++ discord.sh
@@ -6,14 +6,13 @@
 shopt -s lastpipe   # avoid subshell weirdness hopefully
 shopt -so pipefail  # hopefully correctly get $? in substitution
 
+get_ts() { echo "$(date -u --iso-8601=seconds)"; }
+
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 webhook_file="${thisdir}/.webhook"
 
-# gotta have a command, boss
-[[ "$#" -eq 0 ]] && echo "fatal: no command given" && exit 1
-
-# grab command
-cmd="${1}"; shift;
+# HELP TEXT PLEASE
+[[ "$#" -eq 0 ]] && echo "help text goes here" && exit 0
 
 # gather arguments
 while (( "$#" )); do
@@ -31,22 +30,39 @@ while (( "$#" )); do
         --text=*) text=${1/--text=/''}; shift;;
         --text*) text=${2}; shift; shift;;
 
-        --avatar-url=*) avatar_url=${1/--avatar-url=/''}; shift;;
-        --avatar-url*) avatar_url=${2}; shift; shift;;
+        --avatar=*) avatar_url=${1/--avatar=/''}; shift;;
+        --avatar*) avatar_url=${2}; shift; shift;;
 
         # embed goodies
 
-        --embed-title=*) embed_title=${1/--embed-title=/''}; shift;;
-        --embed-title*) embed_title=${2}; shift; shift;;
+        --title=*) embed_title=${1/--title=/''}; embedding=1; shift;;
+        --title*) embed_title=${2}; embedding=1; shift; shift;;
 
-        --embed-description=*) embed_description=${1/--embed-description=/''}; shift;;
-        --embed-description*) embed_description=${2}; shift; shift;;
+        --description=*) embed_description=${1/--description=/''}; embedding=1; shift;;
+        --description*) embed_description=${2}; embedding=1; shift; shift;;
 
-        --embed-url=*) embed_url=${1/--embed-url=/''}; shift;;
-        --embed-url*) embed_url=${2}; shift; shift;;
+        --url=*) embed_url=${1/--url=/''}; embedding=1; shift;;
+        --url*) embed_url=${2}; embedding=1; shift; shift;;
 
-        --embed-color=*) embed_color=${1/--embed-color=/''}; shift;;
-        --embed-color*) embed_color=${2}; shift; shift;;
+        --color=*) embed_color=${1/--color=/''}; embedding=1; shift;;
+        --color*) embed_color=${2}; embedding=1; shift; shift;;
+
+        --timestamp*) embed_timestamp=1; shift;;
+
+        # embed author
+
+        --author-name=*) embed_authorname=${1/--author-name=/''}; embedding=1; shift;;
+        --author-name*) embed_authorname=${2}; embedding=1; shift; shift;;
+
+        --author-url=*) embed_authorurl=${1/--author-url=/''}; embedding=1; shift;;
+        --author-url*) embed_authorurl=${2}; embedding=1; shift; shift;;
+
+        --author-icon=*) embed_authoricon=${1/--author-icon=/''}; embedding=1; shift;;
+        --author-icon*) embed_authoricon=${2}; embedding=1; shift; shift;;
+
+        # unknown argument. bail out
+
+        *) echo "fatal: unknown argument '${1}'"; exit 1;;
 
     esac
 done
@@ -60,98 +76,98 @@ done
 
 
 ##
-# build a raw message. output to stdout, to be captured by command substitution
+# build embed author object
 ##
-build_message() {
-    # ensure text is given
-    [[ -z "${text}" ]] && echo "fatal: no text given" && exit 1
+build_author() {
+    # don't build if not embedding
+    [[ -z "${embedding}" ]] && exit;
+    [[ "${embedding}" -ne 1 ]] && exit;
 
-    # wait for confirmation from server
-    local _wait="\"wait\": true"
+    [[ -n "${embed_authorname}" ]] && local _name=", \"name\": \"${embed_authorname}\""
+    [[ -n "${embed_authorurl}" ]] && local _url=", \"url\": \"${embed_authorurl}\""
+    [[ -n "${embed_authoricon}" ]] && local _icon=", \"icon_url\": \"${embed_authoricon}\""
 
-    # these should already be set! we've checked above that they are
-    [[ -n "${username}" ]] && local _username=", \"username\": \"${username}\""
-    [[ -n "${text}" ]] && local _content=", \"content\": \"${text}\""
-    [[ -n "${avatar_url}" ]] && local _avatar=", \"avatar_url\": \"${avatar_url}\""
-    [[ -n "${is_tts}" ]] && local _tts=", \"tts\": true"
-
-    # build message and echo
-    local _json="${_username}${_content}${_avatar}${_tts}"
-    echo "{ ${_wait}${_json} }"
+    echo ", \"author\": { \"placeholder\": \"hello\"${_name}${_url}${_icon} }"
 }
 
 
 ##
-# build an embed object, same as build_message, except this time with feeling
+# build an embed object
 ##
 build_embed() {
+    # should we embed? if not, bail out without error
+    [[ -z "${embedding}" ]] && exit;
+    [[ "${embedding}" -ne 1 ]] && exit;
 
-    # need to have SOMETHING to build
-    [[ -z "${embed_title}" ]] && \
-        [[ -z "${embed_description}" ]] && \
-        [[ -z "${embed_url}" ]] && \
-        [[ -z "${embed_color}" ]] && \
-        echo "fatal: nothing to embed" && exit 1
-
-    # strip 0x prefix and convert hex to dec if necessary
-    [[ -n "${embed_color}" ]] && [[ "${embed_color}" =~ ^0x[0-9a-fA-F]+$ ]] && embed_color=$(( 16#${embed_color/0x/''} ))
-
-    # embed color must be an integer, if given
-    [[ -n "${embed_color}" ]] && ! [[ "${embed_color}" =~ ^[0-9a-fA-F]+$ ]] && \
-        echo "fatal: illegal color '${embed_color}'" && exit 1
-
-    # let's build, boys
-    [[ -n "${username}" ]] && local _username=", \"username\": \"${username}\""
-    [[ -n "${avatar_url}" ]] && local _avatar=", \"avatar_url\": \"${avatar_url}\""
     [[ -n "${embed_title}" ]] && local _title=", \"title\": \"${embed_title}\""
     [[ -n "${embed_description}" ]] && local _desc=", \"description\": \"${embed_description}\""
     [[ -n "${embed_url}" ]] && local _url=", \"url\": \"${embed_url}\""
     [[ -n "${embed_color}" ]] && local _color=", \"color\": ${embed_color}"
+    [[ -n "${embed_timestamp}" ]] && [[ "${embed_timestamp}" -eq 1 ]] && local _ts=", \"timestamp\": \"$(get_ts)\""
 
-    local _embedPrefix="\"nonce\": \"made with <3 by ChaoticWeg and Suce\""
-    local _embed="[{ ${_embedPrefix}${_desc}${_url}${_color} }]"
-    local _prefix="\"wait\": true, \"content\": \"${text}\"${_username}${_avatar}"
-    echo "{ ${_prefix}, \"embeds\": ${_embed} }"
+    local _author="$(build_author)"
+
+    echo ", \"embeds\": [{ \"top\": \"kek\"${_title}${_desc}${_url}${_color}${_ts}${_author} }]"
+}
+
+
+build() {
+
+    # need to have SOMETHING to build
+    [[ -z "${text}" ]] && \
+        [[ -z "${embed_title}" ]] && \
+        [[ -z "${embed_description}" ]] && \
+            echo "fatal: nothing to build" && exit 1
+
+    # strip 0x prefix and convert hex to dec if necessary
+    [[ -n "${embed_color}" ]] && [[ "${embed_color}" =~ ^0x[0-9a-fA-F]+$ ]] && embed_color="$(( ${embed_color} ))"
+
+    # embed color must be an integer, if given
+    [[ -n "${embed_color}" ]] && ! [[ "${embed_color}" =~ ^[0-9]+$ ]] && \
+        echo "fatal: illegal color '${embed_color}'" && exit 1
+
+    # let's build, boys
+
+    [[ -n "${text}" ]] && local _content=", \"content\": \"${text}\""
+    [[ -n "${username}" ]] && local _username=", \"username\": \"${username}\""
+    [[ -n "${avatar_url}" ]] && local _avatar=", \"avatar_url\": \"${avatar_url}\""
+    [[ -n "${embedding}" ]] && local _embed="$(build_embed)"
+
+    local _prefix="\"wait\": true${_content}${_username}${_avatar}${_embed}"
+
+    echo "{ ${_prefix}${_embed} }"
 }
 
 ##
 # send something to the text channel
 ##
-send_something()
+send()
 {
     # gotta send something
-    [[ -z "${1}" ]] && echo "fatal: give me something to send" && exit 1
+    [[ -z "${1}" ]] && echo "fatal: give me something to send" && exit 1;
+
+    local _sendme="${1}"
 
     # dry run?
-    [[ -n ${is_dry} ]] && [[ "${is_dry}" -ne 0 ]] && echo "${_content}" && exit 0;
+    [[ -n "${is_dry}" ]] && [[ "${is_dry}" -ne 0 ]] && echo "${1}" && exit 0;
 
     # make the POST request and parse the results
     # results should be empty if there's no problem. otherwise, there should be code and message
     local _result
 
-     _result=$(curl -H "Content-Type: application/json" -X POST "${webhook_url}" -d "${1}" 2>/dev/null)
+     _result=$(curl -H "Content-Type: application/json" -X POST "${webhook_url}" -d "${_sendme}" 2>/dev/null)
      send_ok=$?
      [[ "${send_ok}" -ne 0 ]] && echo "fatal: curl failed with code ${send_ok}" && exit $send_ok
 
      _result=$(echo "${_result}" | jq '.')
 
     # if we have a result, there was a problem. echo and exit.
-    [[ -n ${_result} ]] && \
-        echo error: "$(echo "${_result}" | jq .message)" \("$(echo "${_result}" | jq .code)"\) && \
+    [[ -n "${_result}" ]] && \
+        echo error! "${_result}" && \
+        echo attempted to send: $(echo "${_sendme}" | jq '.') && \
         exit 1
 
     exit 0
 }
 
-
-##
-# process command
-##
-
-case "${cmd}" in
-    say) if ! _content=$(build_message); then echo "${_content}"; exit 1; else send_something "${_content}"; exit $?; fi;;
-    embed) if ! _content=$(build_embed); then echo "${_content}"; exit 1; else send_something "${_content}"; exit $?; fi;;
-
-    *) echo "fatal: unrecognized command '${cmd}'"; exit 1;;
-esac
-
+if target=$(build); then send "${target}"; exit 0; else echo "${target}"; exit 1; fi
