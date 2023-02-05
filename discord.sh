597baf18924feff1b4792189b9db41e0e215d107
Sheldon Rupp <hello@sheldon.is>
2023-02-05 21:29:51 +0100

    Update copyright notice within script

--- discord.sh
+++ discord.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 #
 # Discord.sh - Discord on command-line
-# by ChaoticWeg and fieu
+# Copyright (C) 2023 ChaoticWeg & fieu
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 shopt -s lastpipe   # avoid subshell weirdness hopefully
 shopt -so pipefail  # hopefully correctly get $? in substitution
