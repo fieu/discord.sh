29506631c8f0c36b9c993324955f7d268d5dfe9d
Sheldon Rupp <hello@sheldon.is>
2023-02-06 12:45:41 +0100

    Remove changelog scripts

--- scripts/changelog.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-auto-changelog -t "$DIR/changelog-template.hbs" -l 999 --starting-version "$(git describe --abbrev=0)" --stdout --sort-commits date-desc --hide-credit
