2aeccb3f81be77791c5b421068101f3b3d4f1e3a
Sheldon Rupp <hello@sheldon.is>
2025-10-27 09:28:32 +0100

    chore: year bump

--- discord.sh
+++ discord.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Discord.sh - Discord on command-line
-# Copyright (C) 2023 ChaoticWeg & fieu
+# Copyright (C) 2025 ChaoticWeg & fieu
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
